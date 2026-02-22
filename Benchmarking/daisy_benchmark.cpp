93752a7e7dcd75ae1468c974c88aef28ef36fad9
Kai <kaimul24@icloud.com>
2026-02-21 20:44:10 -0800

    cont. benchmarking daisy

--- Benchmarking/daisy_benchmark.cpp
+++ Benchmarking/daisy_benchmark.cpp
@@ -3,40 +3,54 @@
 
 using namespace daisy;
 
-constexpr size_t BLOCK_SIZE = 4;
+constexpr size_t BLOCK_SIZE = 16;
 constexpr auto SAMPLE_RATE = SaiHandle::Config::SampleRate::SAI_48KHZ;
 
 DaisyPod hw;
 CpuLoadMeter cpu_load;
 Passthrough effect;
 
+/*
+* TODO:
+* - Find guitar sample (5s) and place in Flash for common benchmarking input
+*
+*
+*/ 
+
 void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size) {
 	cpu_load.OnBlockStart();
-	hw.ProcessAllControls();
+	// Don't allow use of Pod knobs + rotary encoder to keep benchmarking fair
+	// hw.ProcessAllControls(); 
 	effect.process_audio(in, out, size);
 	cpu_load.OnBlockEnd();
 }
 
-int main(void)
-{
+int main(void) {
 	hw.Init();
-	hw.seed.StartLog();
+	hw.seed.StartLog(true);
 	hw.SetAudioBlockSize(BLOCK_SIZE);
 	hw.SetAudioSampleRate(SAMPLE_RATE);
 	cpu_load.Init(hw.AudioSampleRate(), hw.AudioBlockSize());
 	hw.StartAdc();
 	hw.StartAudio(AudioCallback);
 
+	int counter = 0;
+
 	while(1) {
-		const float avgLoad = cpu_load.GetAvgCpuLoad();
-		const float maxLoad = cpu_load.GetMaxCpuLoad();
-		const float minLoad = cpu_load.GetMinCpuLoad();
-
-		hw.seed.PrintLine("Processing Load %:");
-		hw.seed.PrintLine("Max: " FLT_FMT3, FLT_VAR3(maxLoad * 100.0f));
-		hw.seed.PrintLine("Avg: " FLT_FMT3, FLT_VAR3(avgLoad * 100.0f));
-		hw.seed.PrintLine("Min: " FLT_FMT3, FLT_VAR3(minLoad * 100.0f));
-
-		System::Delay(500);
+		while (counter < 5) {
+			const float avgLoad = cpu_load.GetAvgCpuLoad();
+			const float maxLoad = cpu_load.GetMaxCpuLoad();
+			const float minLoad = cpu_load.GetMinCpuLoad();
+			
+			hw.seed.PrintLine("Processing Load %:");
+			hw.seed.PrintLine("Max: " FLT_FMT3, FLT_VAR3(maxLoad * 100.0f));
+			hw.seed.PrintLine("Avg: " FLT_FMT3, FLT_VAR3(avgLoad * 100.0f));
+			hw.seed.PrintLine("Min: " FLT_FMT3, FLT_VAR3(minLoad * 100.0f));
+			hw.seed.PrintLine("");
+			counter++;
+			System::Delay(500);
+			
+		} 
+		
 	}
 }
--- Benchmarking/effects.h
+++ Benchmarking/effects.h
@@ -7,21 +7,96 @@ using namespace daisy;
 
 struct BaseEffect {
     BaseEffect() = default;
-
     virtual ~BaseEffect() = default;
-    virtual void process_audio(AudioHandle::InputBuffer &in,
-                                AudioHandle::OutputBuffer &out,
-                                size_t size) = 0;
+    virtual void process_audio(AudioHandle::InputBuffer in,
+                               AudioHandle::OutputBuffer out,
+                               size_t size) = 0;
 };
 
 struct Passthrough : BaseEffect {
-    void process_audio(AudioHandle::InputBuffer &in,
-                       AudioHandle::OutputBuffer &out, 
+    inline void process_audio(AudioHandle::InputBuffer in,
+                       AudioHandle::OutputBuffer out, 
                        size_t size) override {
 
         for (size_t i = 0; i < size; i++) {
             out[0][i] = in[0][i];
             out[1][i] = in[1][i];
 	    }
+    }
+};
+
+/*
+Low-Complexity Effects
+*/
+
+struct Distorion : BaseEffect {
+    inline void process_audio(AudioHandle::InputBuffer in,
+                       AudioHandle::OutputBuffer out, 
+                       size_t size) override {
+    }
+};
+
+struct SynthSine : BaseEffect {
+    inline void process_audio(AudioHandle::InputBuffer in,
+                       AudioHandle::OutputBuffer out, 
+                       size_t size) override {
+    }
+};
+
+struct C : BaseEffect {
+    inline void process_audio(AudioHandle::InputBuffer in,
+                       AudioHandle::OutputBuffer out, 
+                       size_t size) override {
+    }
+};
+
+/*
+Medium-Complexity Effects
+*/
+
+struct Delay : BaseEffect {
+    inline void process_audio(AudioHandle::InputBuffer in,
+                       AudioHandle::OutputBuffer out, 
+                       size_t size) override {
+    }
+};
+
+struct Chorus : BaseEffect {
+    inline void process_audio(AudioHandle::InputBuffer in,
+                       AudioHandle::OutputBuffer out, 
+                       size_t size) override {
+    }
+};
+
+struct C : BaseEffect {
+    inline void process_audio(AudioHandle::InputBuffer in,
+                       AudioHandle::OutputBuffer out, 
+                       size_t size) override {
+    }
+};
+
+/*
+High-Complexity Effects
+*/
+
+struct Reverb : BaseEffect {
+    inline void process_audio(AudioHandle::InputBuffer in,
+                       AudioHandle::OutputBuffer out, 
+                       size_t size) override {
+    }
+};
+
+
+struct C : BaseEffect {
+    inline void process_audio(AudioHandle::InputBuffer in,
+                       AudioHandle::OutputBuffer out, 
+                       size_t size) override {
+    }
+};
+
+struct D : BaseEffect {
+    inline void process_audio(AudioHandle::InputBuffer in,
+                       AudioHandle::OutputBuffer out, 
+                       size_t size) override {
     }
 };
